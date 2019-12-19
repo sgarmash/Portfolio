e0df0ef18f2e14e2336d6e9f733ebadfb23950c2
sergey09 <sergey.garmash09@gmail.com>
2019-12-19 07:40:09 +0200

    feat(layout): added base structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/reset-css/sass/_reset.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
